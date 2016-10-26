c4d5671933f3d14cf54b79ef03a564a8745c5683
Rey Ramos <ramor11@labcorp.com>
2016-10-26 16:30:42 -0400

    update

--- example/src/main/index.js
+++ example/src/main/index.js
@@ -1,107 +1,58 @@
 (function () {
-  'use strict'
+    'use strict'
 
-  const path = require('path')
-  const electron = require('electron')
-  const app = electron.app
-  const Menu = electron.Menu
-  const Tray = electron.Tray
-  const BrowserWindow = electron.BrowserWindow
+    const path = require('path')
 
-  let tray = null
-  let win = null
-  let quitting = false
+    const {app, remote, BrowserWindow, ipcMain, autoUpdater} = require('electron');
 
-  const args = require('./args')
-  const squirrel = require('./squirrel')
 
-  const cmd = args.parseArguments(app, process.argv.slice(1)).squirrelCommand
-  if (process.platform === 'win32' && squirrel.handleCommand(app, cmd)) {
-    return
-  }
+    let tray = null
+    let win = null
+    let quitting = false
 
-  const createMenu = () => {
-    const appMenu = Menu.buildFromTemplate([
-      {
-        label: 'File',
-        submenu: [
-          {
-            label: 'Quit',
-            accelerator: 'CmdOrCtrl+Q',
-            click: () => {
-              app.quit()
-            }
-          }
-        ]
-      }
-    ])
-    Menu.setApplicationMenu(appMenu)
-  }
+    const args = require('./args')
+    const squirrel = require('./squirrel')
 
-  const createTray = () => {
-    const variant = (process.platform === 'darwin' ? 'Black' : 'White')
-    const iconPath = path.resolve(__dirname, `../../resources/Icon${variant}Template.png`)
-
-    tray = new Tray(iconPath)
-
-    const trayMenu = Menu.buildFromTemplate([
-      {
-        label: 'Preferences...',
-        click: () => {
-          win.show()
-        }
-      },
-      {
-        type: 'separator'
-      },
-      {
-        label: 'Quit',
-        click: () => {
-          app.quit()
-        }
-      }
-    ])
-    tray.setContextMenu(trayMenu)
-  }
-
-  const createWindow = () => {
-    const iconPath = path.resolve(__dirname, '../../resources/Icon.png')
-    const winUrl = 'file://' + path.resolve(__dirname, '../renderer/index.html')
-
-    win = new BrowserWindow({
-      width: 800,
-      height: 600,
-      show: false,
-      icon: iconPath
-    })
-    win.loadURL(winUrl)
-
-    win.on('close', (evt) => {
-      if (quitting) {
+    const cmd = args.parseArguments(app, process.argv.slice(1)).squirrelCommand
+    if (process.platform === 'win32' && squirrel.handleCommand(app, cmd)) {
         return
-      }
+    }
 
-      evt.preventDefault()
-      win.hide()
+
+    const createWindow = () => {
+        let params = {
+            resizable: true,
+            autoHideMenuBar: true,
+            webPreferences: {
+                webSecurity: false,
+                allowDisplayingInsecureContent: true,
+                allowRunningInsecureContent: true,
+            }
+        };
+        let win = new BrowserWindow(params);
+
+        win.loadURL("https://reymundoramos.com");
+
+        win.on('close', (evt) => {
+            if (quitting) {
+                return
+            }
+
+            evt.preventDefault()
+            win.hide()
+        })
+
+        win.on('closed', () => {
+            tray = null
+            win = null
+        })
+    }
+
+    app.on('before-quit', () => {
+        quitting = true
+    }).on('window-all-closed', () => {
+        app.quit()
+    }).on('ready', () => {
+        createWindow()
     })
-
-    win.on('closed', () => {
-      tray = null
-      win = null
-    })
-  }
-
-  app.on('before-quit', () => {
-    quitting = true
-  })
-
-  app.on('window-all-closed', () => {
-    app.quit()
-  })
-
-  app.on('ready', () => {
-    createMenu()
-    createTray()
-    createWindow()
-  })
-})()
+})();
